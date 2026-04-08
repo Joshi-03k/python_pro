335ea86687ef0854f0e29a0cf033d0bc3e7cee9f
4081Kruti Joshi <krutiajoshi0311@gmail.com>
2026-04-08 14:35:05 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,17 @@
+#to swapping values of variable
+#to create fibonaccie series up to enterd term
+#to check given number is armstrong number
+#to check prime numbers
+#to check given number is divisible by 7
+
+#1.swapping values
+
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+
+a,b=b,a
+
+print("after swapping")
+print("a",a)
+print("b",b)
+
